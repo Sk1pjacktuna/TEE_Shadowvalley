a7441750db486f65046730f8a54ab79dea1f243d
florinflorinflorin <>
2024-10-16 14:12:37 +0200

    Update balzibalzi.R

--- balzibalzi.R
+++ balzibalzi.R
@@ -12,16 +12,25 @@ simulate_one_gen <- function(N_a, N_A, fitnessa, fitnessA, mut_rate) {
   
   return(c(N_a_new, N_A_new))
 }
-
 simulate_pop <- function(N_init_a, N_init_A, maxfit, minfit, fitflip, mut_rate, t_max) {
   # Create the vector in which to save the results
   pop_vector <- c(N_init_a, N_init_A)
   # initiate the variables
   pop_new <- c(N_init_a, N_init_A)
   v <- 0
+  fitnessa <- minfit
+  fitnessA <- maxfit
   # run the simulation until generation t_max
   for (i in 1:t_max+1) {
-    #define fitness of a and A for each 
+    #define fitness of a and A for each iteration
+    if (v<fitflip){
+      v <- v+1
+    } else {
+      fitness <- fitnessa
+      fitnessa <- fitnessA
+      fitnessA <- fitness
+    }
+      
     # redefine the current population one generation later
     pop_new <- simulate_one_gen(pop_new[1],pop_new[2], fitnessa, fitnessA, mut_rate)
     # add the new population sizes to the output vector
@@ -36,3 +45,19 @@ simulate_pop <- function(N_init_a, N_init_A, maxfit, minfit, fitflip, mut_rate, 
   # return the result
   return(pop_vector)	
 }
+
+max_gen <- 1000
+# create your simulation data
+output <- simulate_pop(1000,0,1.2,0.9,15,0.001,max_gen)
+
+# show the last few lines of the data table
+print(tail(output))
+# plot the output - note that the plot range and the x axis have to be adjusted based on the length of the table
+# determine x axis range 
+x_range <- 0:(length(output[,1])-1)
+# this plots the total population size
+plot(x_range,output[,1]+output[,2],type='l',ylim=c(0,max(output[,1]+output[,2])),xlab = "Generation",ylab = "Population size")
+# add number of wild type individuals
+lines(x_range,output[,1], col="blue")
+# add number of mutant individuals
+lines(x_range,output[,2], col="red")
