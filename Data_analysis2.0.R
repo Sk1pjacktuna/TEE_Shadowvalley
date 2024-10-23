3f9eb1b3cd5afb51f45469b5b3132aa0dac6b7d8
Sk1pjacktuna <felix.rentschler@gmail.com>
2024-10-23 08:56:42 +0200

    I dont understandddddd"_"

--- Data_analysis2.0.R
+++ Data_analysis2.0.R
@@ -55,7 +55,7 @@ simulate_pop_switching_migrants <- function(N0_a, N0_A, decay_rate, sel_coeff, f
 
 ##########################averages##################
 
-simulate_one_gen_hardy_weinberg <- function(N_aa,N_Aa, N_AA, fitnessaa, fitnessAa, fitnessAA, mut_rate) {
+simulate_one_gen_hardy_weinberg <- function(N_aa,N_Aa, N_AA, fitnessaa, fitnessAa, fitnessAA, avgmigrants, mut_rate) {
   # calculate total pop size
   tot_pop <- N_aa+N_Aa+N_AA
   # calculate average fitness
@@ -63,27 +63,53 @@ simulate_one_gen_hardy_weinberg <- function(N_aa,N_Aa, N_AA, fitnessaa, fitnessA
   # calculate deterministic next gen pop size
   next_gen_tot_pop <- tot_pop*avg_fit
   # calculate the allele frequnecies
-  p_tot <- N_aa+0.5*N_Aa
-  q_tot <- N_AA+0.5*N_Aa
-  pmut <- rpois(1,p_tot*mut_rate)
-  qmut <- rpois(1,q_tot*mut_rate)
-  p <- (p_tot+qmut-pmut)/tot_pop
-  q <- (q_tot+pmut-qmut)/tot_pop
-  # Next generation genotype frequencies
+  a_tot <- 2*N_aa+N_Aa
+  A_tot <- 2*N_AA+N_Aa
+  a_to_A_mut <- rpois(1,a_tot*mut_rate)
+  A_to_a_mut <- rpois(1,A_tot*mut_rate)
+  p <- (a_tot+A_to_a_mut-a_to_A_mut)/(2*tot_pop)
+  q <- (A_tot+a_to_A_mut-A_to_a_mut)/(2*tot_pop)
+  # Next generation genotype frequencies; selection is applied here
   det_aa_next <- (p^2)*fitnessaa/avg_fit
   det_Aa_next <- (2*p*q)*fitnessAa/avg_fit
   det_AA_next <- (q^2)*fitnessAA/avg_fit
   
+  #migrationrate
+  aa_migrants <- rpois(1, avgmigrants)
+  
   # draw offspring according to Poisson distribution
-  offsp_aa <- rpois(1, next_gen_tot_pop*det_aa_next)
+  offsp_aa <- rpois(1, next_gen_tot_pop*det_aa_next) + avgmigrants
   offsp_Aa <- rpois(1, next_gen_tot_pop*det_Aa_next)
   offsp_AA <- rpois(1, next_gen_tot_pop*det_AA_next)
   
   
   return(c(offsp_aa, offsp_Aa,offsp_AA))
 }
+simulate_pop_HW <- function(N_init_aa, N_init_Aa, N_init_AA, fitnessaa, fitnessAa, fitnessAA, avgmigrants, mut_rate, t_max) {
+  # Create the vector in which to save the results
+  pop_vector <- c(N_init_aa, N_init_Aa, N_init_AA)
+  # initiate the variables
+  pop_new <- c(N_init_aa, N_init_Aa, N_init_AA)
+  v <- 0
+  # run the simulation until generation t_max
+  for (i in 1:t_max+1) {
+    #define fitness of a and A for each iteration
+    # redefine the current population one generation later
+    pop_new <- simulate_one_gen_hardy_weinberg(pop_new[1],pop_new[2], pop_new[3], fitnessaa, fitnessAa, fitnessAA,avgmigrants, mut_rate)
+    # add the new population sizes to the output vector
+    pop_vector <- rbind(pop_vector,pop_new)
+    # condition to stop the simulation before t_max: either the population exceeds 1.5 times the original population size, or it goes extinct
+    if (pop_new[1]+pop_new[2]+pop_new[3]>=5*(N_init_aa+ N_init_Aa+ N_init_AA) | pop_new[1]+pop_new[2]==0) break
+  }
+  
+  # define the row and column names of the output vector
+  rownames(pop_vector) <- (0:t_max)[1:length(pop_vector[,1])] # note that the vector has to be cut if the simulation stopped early
+  colnames(pop_vector) <- c("aa","Aa","AA")
+  # return the result
+  return(pop_vector)	
+}
+
 # Test the function
-print(simulate_one_gen_hardy_weinberg(100,0,100,0.3,0.3,1,0.01))
 
 # set some parameters to fixed values
 #init_a <- 1000
@@ -95,28 +121,28 @@ print(simulate_one_gen_hardy_weinberg(100,0,100,0.3,0.3,1,0.01))
 #igration_min <- 0.1
 #migration_max <- 5
 #mut_rate <- 0
-N_aa <- 100
+N_aa <- 1000
 N_Aa <- 0
 N_AA <- 100
 fitnessaa <- 0.3
 fitnessAa <- 0.3
 fitnessAA <- 1
+avgmigrants <- 4
 mut_rate <- 0.01
+t_max <- 100
 
 
-one_run <- simulate_one_gen_hardy_weinberg(N_aa,N_Aa, N_AA, fitnessaa, fitnessAa, fitnessAA, mut_rate)
+one_run <- simulate_one_gen_hardy_weinberg(N_aa,N_Aa, N_AA, fitnessaa, fitnessAa, fitnessAA, avgmigrants, mut_rate)
 
 # determine how often to run the simulation for each set of parameters
 no_replicates <- 100
-
-# set parameters to vary
-switch_gens <- c(1,3,5,10,15)
 
 # initialize data table - where to collect the results
 data_table <- c()
 # run the simulation across all chosen parameters
 # loop over switch generations
 migration_rates <- c(1:10)
+
 for(migration_rate in migration_rates){
     # reset counter
     i<-1
@@ -124,7 +150,7 @@ for(migration_rate in migration_rates){
       # increase counter by one
       i<-i+1
       # run the simulation once
-      one_run <- simulate_one_gen_hardy_weinberg(N_aa,N_Aa, N_AA, fitnessaa, fitnessAa, fitnessAA, mut_rate)
+      one_run <- simulate_one_gen_hardy_weinberg(N_aa,N_Aa, N_AA, fitnessaa, fitnessAa, fitnessAA, avgmigrants, mut_rate)
       # determine total population sizes
       total_size <- one_run[1] + one_run[2] + one_run[3]
       #number of generation at which 1.5*n0 is achieved
@@ -140,6 +166,8 @@ for(migration_rate in migration_rates){
     }
     migration_rate <- migration_rate + 1
 }
+
+
 # colnames(data_table) <- c("flipswitch","min_gen","min_size","number_of_generations")
 
 print(one_run)
